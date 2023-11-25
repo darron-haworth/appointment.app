c661caf66d2a9778ebcbd09f2c9a7935342cbbb2
Darron Haworth <apple@fivepals.com>
2023-11-24 16:48:25 -0800

    simple object to obj array with two way data binding

--- src/app/appointment-list/appointment-list.component.ts
+++ src/app/appointment-list/appointment-list.component.ts
@@ -8,5 +8,22 @@ import { Appointment } from '../models/appointment';
 })
 export class AppointmentListComponent {
 
-  appointment: Appointment =  { id: 1, title: "Take dog for a walk", date: new Date('2023-07-30')}
+  newAppointmentTitle: string = "";
+  newAppointmentDate: Date = new Date();
+
+  appointments: Appointment[] = []
+
+  addAppointment() {
+    if (this.newAppointmentTitle.trim().length && this.newAppointmentDate) {
+      let newAppointment: Appointment = { 
+        id: Date.now(), 
+        title: this.newAppointmentTitle, 
+        date: this.newAppointmentDate 
+      }
+      this.appointments.push(newAppointment)
+      this.newAppointmentTitle = "";
+      this.newAppointmentDate= new Date()
+    }
+  }
+
 }
